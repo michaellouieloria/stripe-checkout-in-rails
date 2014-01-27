59ef64f35bb40fd3fc16cdf1c64457f6c854e6ff
Michael Louie Loria <michaellouieloria@gmail.com>
2014-01-27 15:43:20 +0800

    initial import

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-stripe-checkout-in-rails
-========================
-
 Stripe’s Checkout and Rails
+============
+
+Using Stripe’s Checkout and Rails
+
+Based from [Stripe's Guide](https://stripe.com/docs/checkout/guides/rails)
+
+1. Enter secret and publishable key in [config\initializers\stripe.rb](https://github.com/michaellouieloria/stripe-checkout-in-rails/blob/master/config/initializers/stripe.rb)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stripe1::Application.config.secret_key_base = '810719812907fd3e33e2d08cf8150e669a0f4c18f132d9240b445253de64e76806375fc63e6d92578c6c57f426e2d636b0c4ae279d71e8b4696afc2cafc5d12b'
